ba8362ddd6bfbd7fc9c0abee9d9b60958bbe5a55
= <pranavkumar0155@gmail.com>
2024-03-22 08:27:40 +0530

    added responsiveness to full blog part

--- frontend/src/components/FullBlog.tsx
+++ frontend/src/components/FullBlog.tsx
@@ -15,14 +15,14 @@ export const FullBlog = ({ blog }: { blog: Blog }) => {
 
   return (
     <div>
-      <Appbar />
+      <Appbar />{" "}
       <div className="flex justify-center">
         <div className="grid grid-cols-12 px-10 w-full pt-200 max-w-screen-xl pt-12">
-          <div className="col-span-8">
+          <div className="md:col-span-8 col-span-12">
             <div className="text-5xl font-extrabold">{blog.title}</div>
             <CodeViewer content={blog.content} language="cpp" />
           </div>
-          <div className="col-span-4 ml-10">
+          <div className="md:col-span-4 col-span-12 ml-10">
             <div className="text-slate-600 text-lg">Author</div>
             <div className="flex w-full">
               <div className="pr-4 flex flex-col justify-center">
